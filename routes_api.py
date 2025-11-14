da86bbfdb84710d1d1736ff85b740bdeb4bb3245
Jkhananisho <khananishojoseph@gmail.com>
2025-11-13 19:57:11 -0600

    finished routes api file

--- routes_api.py
+++ routes_api.py
@@ -7,11 +7,11 @@ def get_route_options(origin_zip, destination_zip):
     dest_coords = zip_to_coords(destination_zip)
 
     if origin_coords is None or dest_coords is None:
-        return ( 
+        return { 
             "route_name": "Unknown",
             "distance_mi": 0,
             "estimated_time_min": 0
-        )
+        }
 
     lat1, lon1 = origin_coords
     lat2, lon2 = dest_coords
@@ -22,3 +22,30 @@ def get_route_options(origin_zip, destination_zip):
         "mode": "drive",
         "apiKey": GEOAPIFY_API_KEY
     }
+
+    try: 
+        response = requests.get(url, params=params)
+        data = response.json()
+
+        feature = data["features"][0]
+        props = feature["properties"]
+        summary = props["summary"]
+
+        distance_m = summary["distance"]
+        duration_s = summary["duration"]
+
+        distance_mi = distance_m * 0.000621371
+        duration_min = duration_s / 60
+
+        return {
+            "route_name": "Driving Route",
+            "distance_mi": round(distance_mi, 1),
+            "estimated_time_min": round(duration_min)
+        }
+    
+    except:
+        return {
+            "route_name": "Unknown",
+            "distance_mi": 0,
+            "estimated_time_min": 0
+        }
